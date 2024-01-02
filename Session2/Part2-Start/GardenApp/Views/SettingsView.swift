b4d1d3c5eaf9e4e2ab3040c6cbb75e5b3c2b14e8
Tim Mitra <tim@it-guy.com>
2024-01-01 21:30:02 -0500

    add Settingsview

--- Session2/Part2-Start/GardenApp/Views/SettingsView.swift
+++ Session2/Part2-Start/GardenApp/Views/SettingsView.swift
@@ -10,15 +10,35 @@ import SwiftUI
 struct SettingsView: View {
 
     var body: some View {
-        Color.clear
-            .frame(width: 400, height: 200, alignment: .top)
+      TabView {
+        GeneralSettings()
+          .tabItem {
+            Label("General", systemImage: "gear")
+          }
+        ViewingSettings()
+          .tabItem {
+            Label("Viewing", systemImage: "eyeglasses")
+          }
+      }
+      .frame(width: 400, height: 200, alignment: .top)
     }
 
     private struct GeneralSettings: View {
-        @EnvironmentObject var store: Store
-
+      @EnvironmentObject var store: Store
+      @AppStorage("defaultGarden") var selection: Garden.ID?
         var body: some View {
-            Color.clear
+          Form {
+            Picker("Default Garden", selection: $selection) {
+              Text("None")
+                .tag(Garden.ID?.none)
+              ForEach(store.gardens) { garden in
+                Text("\(garden.name), \(garden.displayYear)")
+                  .tag(Garden.ID?.some(garden.id))
+              }
+            }
+            .fixedSize()
+          }
+          .padding()
         }
     }
 
